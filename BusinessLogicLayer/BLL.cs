a05f2b215cb29ed898daa42139d5f601e03d8baf
BaThuyen <BaThuyenLe@gmail.com>
2016-08-22 10:35:48 +0700

    sp1

---
+++ BusinessLogicLayer/BLL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class BLL
+    {
+        DAL data = new DAL();
+        public bool dangNhap(string maNV, string matKhau)
+        {
+            return data.dangNhap(maNV, matKhau);
+        }
+    }
+}
